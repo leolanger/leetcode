1688031cc229151932256abf04a1d06f7f78063f
lhd-code-new <1176127450@qq.com>
2020-08-07 17:54:30 +0800

    update

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,8 +1,19 @@
 # include <iostream>
+# include <time.h>
 int main()
 {
     using namespace std;
-    int a = 1;
-    int b = 1;
-    cout << a + b;
+    int a = rand() * 100;
+    int b;
+    do
+    {
+        cin >> b;
+        if (b > a)
+            cout << "bigger" << "try again" << endl;
+        else if (b < a)
+            cout << "smaller" << "try again" << endl;
+
+    } while (a == b);
+    cout << "congratulation!";
+
 }
